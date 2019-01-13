619d38e38ebcb9752feec230ec7211501285fd67
Igor Zolotarev <yngvar.antonsson@gmail.com>
2019-01-13 16:05:14 +0300

    Solution matrix generator added

--- src/task.py
+++ src/task.py
@@ -16,3 +16,11 @@ def u(x1, x2):
 
 def f(u):
     return np.pi**2*u**3
+
+def solution(x1, x2):
+    sol = np.ones((len(x1), len(x2)))
+
+    for i in range(0, len(x1)):
+        for j in range(0, len(x2)):
+            sol[i,j] = u(x1[i],x2[j])
+    return sol
