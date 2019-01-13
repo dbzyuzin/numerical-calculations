424c0f3fdd3e8cc1710c1e6b756a86c9a4d1c3c1
Igor Zolotarev <yngvar.antonsson@gmail.com>
2019-01-13 18:17:22 +0300

    Small fixes

--- src/main.py
+++ src/main.py
@@ -18,4 +18,18 @@ tau, mu = simple_iter_koef(A)
 ns = simple_iter_count(eps, mu)
 A, F = make_koef_matr(ys, (N, N), tau, (h,h))
 
-jacobi(A,F, ns, None, eps)
+X = np.zeros((N*N,1))
+X0 = np.zeros((N*N,1))
+z = 0
+for i in range(0, N):
+    for j in range(0, N):
+        X[z,0] = ys[i,j]
+        X0[z,0]= ytest[i,j]
+        z = z+1
+while all(abs(X0-X)) > eps:
+    X = jacobi(A,F, ns, X, eps)
+    for z in range(0, N*N):
+        ys[z // N, z % N] = X[z]
+    tau, mu = simple_iter_koef(A)
+    ns = simple_iter_count(eps, mu)
+    A, F = make_koef_matr(ys, (N,N), tau, (h,h))
