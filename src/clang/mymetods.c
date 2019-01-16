cf0036af88b38130671b597787c4992f1eb32abe
Igor Zolotarev <yngvar.antonsson@gmail.com>
2019-01-16 22:23:01 +0300

    Some

--- src/clang/mymetods.c
+++ src/clang/mymetods.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include "mymetods.h"
 
+
 double* linspace(int xa, int xb, int N) {
 	double *x = calloc(N, sizeof(double));
     double h = (xb - xa)/(double)(N-1);
@@ -11,7 +12,7 @@ double* linspace(int xa, int xb, int N) {
 	return x;
 }
 
-void print_res(int N1,int N2,double h1,doube h2,double eps,int iter_count,double rka) {
+void print_res(int N1,int N2,double h1, double h2,double eps,int iter_count,double rka) {
     printf("Параметры :\n%7s %7s %7s %7s %8s\n","N1", "N2", "h1", "h2", "eps");
     // printf("\n%7d %7d %7.3f %7.3f %8e\n","N1", "N2", "h1", "h2", "eps");
     // s += "{: ^7d}{: ^7d}{: ^7.3f}{: ^7.3f}{: ^8}\n\n".format(N1, N2, h1, h2, eps)
