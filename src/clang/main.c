12f2980ae18c809f38cb6d741b75894ef57bd497
Dmitry <dbzyuzin@gmail.com>
2019-01-21 10:23:29 +0300

    all work!

--- src/clang/main.c
+++ src/clang/main.c
@@ -29,32 +29,35 @@ int main(int argc, char  *argv[])
     int start_row,last_row,num_row,start_col,last_col,num_col;
     int proc_left,proc_right, proc_down,proc_up;
     int limits1[2], limits2[2];
+    double t1;
 
     int isper[] = {0,0}; //периодичность решетки
     int dim[2]; //размерность
     int coords[2];
-    dim[0] = dim[1] = 1;
     MPI_Request req[8];
     MPI_Status status[8];
     MPI_Comm newcomm;
     MPI_Datatype vectype;
 
     MyNetInit(&argc, &argv, &np, &mp);
+    grid(N1, N2, np, &dim[0], &dim[1]);
     MPI_Cart_create(MPI_COMM_WORLD,2,dim,isper,1,&newcomm);
     MPI_Cart_shift(newcomm,0,1,&proc_up,&proc_down);
     MPI_Cart_shift(newcomm,1,1,&proc_left,&proc_right);
     MPI_Comm_rank (newcomm, &mp); /* my place in MPI system */
     MPI_Cart_coords(newcomm,mp,2,coords);
 
-    // proc_left = proc_right = proc_down = proc_up = 1;
+
+
     /* rows of matrix I have to process */
-    start_row = 0;
-    last_row = N1-1;
+    start_row = (coords[0] * N1) / dim[0];
+    last_row = (((coords[0] + 1) * N1) / dim[0])-1;
     num_row = last_row - start_row + 1;
     /* columns of matrix I have to process */
-    start_col = 0;
-    last_col = N2-1;
+    start_col = (coords[1] * N2) / dim[1];
+    last_col = (((coords[1] + 1) * N2) / dim[1])-1;
     num_col = last_col - start_col + 1;
+
 
     MPI_Type_vector(num_row,1,num_col+2,MPI_DOUBLE,&vectype);
     MPI_Type_commit(&vectype);
@@ -83,6 +86,8 @@ int main(int argc, char  *argv[])
             Cs[i][j] = calloc(5, sizeof(double));
     }
 
+    m0_printf("CALC STARTED on %d*%d processors with %d*%d array, my part %dx%d\n",dim[0],dim[1],N1,N2,num_row,num_col);
+
     for(int i = 1; i < num_row+1; i++)
         for(int j=1; j < num_col+1; j++)
         {
@@ -105,12 +110,17 @@ int main(int argc, char  *argv[])
         }
     memcpy(ys1[0],ys[0], (num_row+2) * (num_col+2)*sizeof(double));
 
+    limits1[0] = (proc_up==MPI_PROC_NULL) ? 2 : 1; limits1[1] = (proc_down==MPI_PROC_NULL) ? num_row : num_row+1;
+    limits2[0] = (proc_left==MPI_PROC_NULL) ? 2 : 1; limits2[1] = (proc_right==MPI_PROC_NULL) ? num_col : num_col+1;
+
+    /****** iteration loop *************************/
+    MPI_Barrier(newcomm);
+    t1 = MPI_Wtime();
     int iter_count;
     for (iter_count = 0; iter_count < maxiter;  iter_count++) {
-        limits1[0] = (proc_up==MPI_PROC_NULL) ? 2 : 1; limits1[1] = (proc_down==MPI_PROC_NULL) ? num_row : num_row+1;
-        limits2[0] = (proc_left==MPI_PROC_NULL) ? 2 : 1; limits2[1] = (proc_right==MPI_PROC_NULL) ? num_col : num_col+1;
+
         for (int i = limits1[0]; i <  limits1[1]; i++) {
-            for (int j = 2; j <  num_col; j++){
+            for (int j = limits2[0]; j <  limits2[1]; j++){
                 Csi = Cs[i][j];
                 Csi[0] = (h2/h1*(ki(ys[i+1][j], ys[i][j]) + ki(ys[i-1][j], ys[i][j])) +
                           + h1/h2*(ki(ys[i][j+1], ys[i][j]) +
@@ -123,16 +133,19 @@ int main(int argc, char  *argv[])
 
                 F[i][j] = h1*h2*f(ys[i][j]);
                 Cs[i][j] = Csi;
+
             }
         }
+
+
+        // printf("%d: %f \n", mp, test_solution(ys, Cs, F, num_row, num_col));
 
         if (test_solution(ys, Cs, F, num_row, num_col) < eps) break;
 
 
         for (int iter_count_j = 0;  iter_count_j < maxiter_jacobi; iter_count_j++) {
-            limits1[0] = (proc_up==MPI_PROC_NULL) ? 2 : 1; limits1[1] = (proc_down==MPI_PROC_NULL) ? num_row : num_row+1;
-            limits2[0] = (proc_left==MPI_PROC_NULL) ? 2 : 1; limits2[1] = (proc_right==MPI_PROC_NULL) ? num_col : num_col+1;
-            #pragma omp parallel for collapse(2)
+
+            // #pragma omp parallel for collapse(2)
             for (int i = limits1[0]; i <  limits1[1]; i++) {
                 for (int j = limits2[0]; j <  limits2[1]; j++){
                     ys1[i][j] = (F[i][j] + Cs[i][j][1]*ys[i+1][j] + Cs[i][j][2]*ys[i-1][j] +
@@ -140,37 +153,44 @@ int main(int argc, char  *argv[])
 
                 }
             }
+
             memcpy(ys[0],ys1[0], (num_row+2) * (num_col+2)*sizeof(double));
-            if (test_solution(ys, Cs, F, num_row, num_col) < eps_j) break;
+            // if (mp==0 && iter_count_j==maxiter_jacobi-1 && iter_count==0){
+            //     for (int i = 0; i <  num_row+2; i++) {
+            //         printf("\n");
+            //         for (int j = 0; j <  num_col+2; j++){
+            //             printf("%6.3f ", ys[i][j]);
+            //
+            //         }
+            //     }
+            //     printf("\n");
+            // }
+            MPI_Irecv(&ys[0][1],num_col,MPI_DOUBLE,
+            proc_up, 1215, MPI_COMM_WORLD, &req[0]);
+            MPI_Isend(&ys[num_row][1],num_col,MPI_DOUBLE,
+            proc_down, 1215, MPI_COMM_WORLD,&req[1]);
+            MPI_Irecv(&ys[num_row+1][1],num_col,MPI_DOUBLE,
+            proc_down, 1216, MPI_COMM_WORLD, &req[2]);
+            MPI_Isend(&ys[1][1],num_col,MPI_DOUBLE,
+            proc_up, 1216, MPI_COMM_WORLD,&req[3]);
+            MPI_Irecv(&ys[1][0],1,vectype,
+            proc_left, 1217, MPI_COMM_WORLD, &req[4]);
+            MPI_Isend(&ys[1][num_col],1,vectype,
+            proc_right, 1217, MPI_COMM_WORLD,&req[5]);
+            MPI_Irecv(&ys[1][num_col+1],1,vectype,
+            proc_right, 1218, MPI_COMM_WORLD, &req[6]);
+            MPI_Isend(&ys[1][1],1,vectype,
+            proc_left, 1218, MPI_COMM_WORLD,&req[7]);
+            MPI_Waitall(8,req,status);
+            // if (test_solution(ys, Cs, F, num_row, num_col) < eps_j) break;
         }
 
-        MPI_Irecv(&ys[0][1],num_col,MPI_DOUBLE,
-        proc_up, 1215, MPI_COMM_WORLD, &req[0]);
-        MPI_Isend(&ys[num_row][1],num_col,MPI_DOUBLE,
-        proc_down, 1215, MPI_COMM_WORLD,&req[1]);
-        MPI_Irecv(&ys[num_row+1][1],num_col,MPI_DOUBLE,
-        proc_down, 1216, MPI_COMM_WORLD, &req[2]);
-        MPI_Isend(&ys[1][1],num_col,MPI_DOUBLE,
-        proc_up, 1216, MPI_COMM_WORLD,&req[3]);
-        MPI_Irecv(&ys[1][0],1,vectype,
-        proc_left, 1217, MPI_COMM_WORLD, &req[4]);
-        MPI_Isend(&ys[1][num_col],1,vectype,
-        proc_right, 1217, MPI_COMM_WORLD,&req[5]);
-        MPI_Irecv(&ys[1][num_col+1],1,vectype,
-        proc_right, 1218, MPI_COMM_WORLD, &req[6]);
-        MPI_Isend(&ys[1][1],1,vectype,
-        proc_left, 1218, MPI_COMM_WORLD,&req[7]);
-        MPI_Waitall(8,req,status);
+
+
     }
 
-    for (int i = 0; i <  num_row+2; i++) {
-        printf("\n");
-        for (int j = 0; j <  num_col+2; j++){
-            printf("%6.3f ", ys[i][j]);
 
-        }
-    }
-    printf("\n");
+
 
     //Задаем точное решение
     solution(x1+start_row, num_row, x2+start_col, num_col, ysol);
@@ -179,7 +199,8 @@ int main(int argc, char  *argv[])
     double yerr = final_error(ys, ysol, num_row, num_col);
 
     // выводим количество итераций и максимальную ошибку
-    print_res(num_row, num_col, h1, h2, eps, iter_count, yerr);
+    printf("\n%d: Time of task=%lf\n",mp,MPI_Wtime()-t1);
+    if (mp==0) print_res(N1, N2, h1, h2, eps, iter_count, yerr);
 
 
     for(int i=0; i<num_row+2; i++)
